c6a7a8a41a6107200fd334543ff0aa4ed7802ee4
Stanislav Hanchev <stanislav@smoothr.de>
2025-10-26 20:32:15 +0200

    add component to error message

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -55,7 +55,7 @@ export default function NoteForm({ onClose }: NoteFormProps) {
                 <div className={css.formGroup}>
                     <label htmlFor="title">Title</label>
                     <Field id="title" type="text" name="title" className={css.input} />
-                    <ErrorMessage name="title" className={css.error} />
+                    <ErrorMessage component='span' name="title" className={css.error} />
                 </div>
 
                 <div className={css.formGroup}>
@@ -67,7 +67,7 @@ export default function NoteForm({ onClose }: NoteFormProps) {
                         rows={8}
                         className={css.textarea}
                     />
-                    <ErrorMessage name="content" className={css.error} />
+                    <ErrorMessage component='span' name="content" className={css.error} />
                 </div>
 
                 <div className={css.formGroup}>
@@ -79,7 +79,7 @@ export default function NoteForm({ onClose }: NoteFormProps) {
                         <option value="Meeting">Meeting</option>
                         <option value="Shopping">Shopping</option>
                     </Field>
-                    <ErrorMessage name="tag" className={css.error} />
+                    <ErrorMessage component='span' name="tag" className={css.error} />
                 </div>
 
                 <div className={css.actions}>
